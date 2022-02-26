b68e0704fbffd3ec4acabd3c0ad376c1a00d1614
IanculescuAlexandra <92098175+IanculescuAlexandra@users.noreply.github.com>
2022-02-26 23:16:46 +0200

    Add files via upload

---
+++ 01_week1/src/ro/ase/cts/g1099/laboratory1/interfaces/Profitable.java
@@ -0,0 +1,9 @@
+package ro.ase.cts.g1099.laboratory1.interfaces;
+
+public interface Profitable {
+
+	//10 -> 10%
+	//0.1 to write percentage - clean code helps us
+	//the interest is the percent value
+	public abstract void addInterest(float interestPercent);
+}
---
+++ 01_week1/src/ro/ase/cts/g1099/laboratory1/main/Main.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.g1099.laboratory1.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		System.out.println("");
+		//implement UML diagrams from ACS
+	}
+
+}
