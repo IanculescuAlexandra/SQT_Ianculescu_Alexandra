be1c5ec2a1c0117563f33fad4cace4ec5f3e73fc
Alexandra Ianculescu <alexa_2000_ian@yahoo.com>
2022-04-03 16:45:56 +0300

    Code formatting - Ensure that proper Java naming conventions (CamelCase)

---
+++ HomeworkA2/src/stage3/Account.java
@@ -0,0 +1,66 @@
+package stage3;
+
+public class Account {
+	public double loanValue, rate;
+	public int daysActive;
+	public AccountType accountType;
+
+	public double loan() {
+		System.out.println("The loan value is " + this.loanValue);
+		return loanValue;
+	}
+
+	public double getRate() {
+		System.out.println("The rate is " + rate);
+		return this.rate;
+	}
+
+	// must have method - the lead has requested it in all classes
+	public double getMonthlyRate() {
+		return loanValue * rate;
+	}
+
+	public void setValue(double value) throws Exception {
+		if (value < 0)
+			throw new Exception();
+		else {
+			loanValue = value;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Loan: " + this.loanValue + "; rate: " + this.rate + "; days active:" + daysActive + "; Type: "
+				+ accountType + ";";
+	}
+
+	public void print() {
+		int vb = 10;
+		System.out.println("This is an account");
+	}
+
+	public static double calculate(Account[] accounts) {
+		double totalFee = 0.0;
+		Account account;
+		int temp = 365;
+		for (int i = 0; i < accounts.length; i++) {
+			account = accounts[i];
+			if (account.accountType == AccountType.PREMIUM || account.accountType == AccountType.SUPER_PREMIUM)
+				totalFee += .0125 * ( // 1.25% broker's fee
+				account.loanValue * Math.pow(account.rate, (account.daysActive / 365)) - account.loanValue); // interest-principal
+		}
+		return totalFee;
+	}
+
+	public Account(double value, double rate, AccountType accountType) throws Exception {
+		if (value < 0)
+			throw new Exception();
+		else {
+			loanValue = value;
+		}
+		this.rate = rate;
+		this.accountType = accountType;
+	}
+
+}
+
---
+++ HomeworkA2/src/stage3/AccountType.java
@@ -0,0 +1,7 @@
+package stage3;
+
+public enum AccountType {
+
+	STANDARD, BUDGET, PREMIUM, SUPER_PREMIUM;
+	
+}
