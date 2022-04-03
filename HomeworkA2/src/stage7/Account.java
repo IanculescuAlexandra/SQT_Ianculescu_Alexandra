55450a38684a976e35ebf7ebc8b54af146a6a17a
Alexandra Ianculescu <alexa_2000_ian@yahoo.com>
2022-04-03 20:06:42 +0300

    Naming conventions

---
+++ HomeworkA2/src/stage7/Account.java
@@ -0,0 +1,68 @@
+package stage7;
+
+public class Account {
+	private double loanValue, rate;
+	private int daysActive;
+	private AccountType accountType;
+
+	public int getDaysActive() {
+		return daysActive;
+	}
+
+	public void setDaysActive(int daysActive) {
+		this.daysActive = daysActive;
+	}
+
+	public AccountType getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(AccountType accountType) {
+		this.accountType = accountType;
+	}
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+
+	public double getLoanValue() {
+		System.out.println("The loan value is " + this.loanValue);
+		return loanValue;
+	}
+
+	public double getRate() {
+		System.out.println("The rate is " + rate);
+		return this.rate;
+	}
+
+	// must have method - the lead has requested it in all classes
+	public double getMonthlyRate() {
+		return loanValue * rate;
+	}
+
+	public void setLoanValue(double value) throws Exception {
+		if (value < 0)
+			throw new Exception();
+		else {
+			loanValue = value;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Loan: " + this.loanValue + "; rate: " + this.rate + "; days active:" + daysActive + "; Type: "
+				+ accountType + ";";
+	}
+
+	public Account(double value, double rate, AccountType accountType) throws Exception {
+		if (value < 0)
+			throw new Exception();
+		else {
+			loanValue = value;
+		}
+		this.rate = rate;
+		this.accountType = accountType;
+	}
+
+}
+
---
+++ HomeworkA2/src/stage7/AccountType.java
@@ -0,0 +1,7 @@
+package stage7;
+
+public enum AccountType {
+
+	STANDARD, BUDGET, PREMIUM, SUPER_PREMIUM;
+	
+}
